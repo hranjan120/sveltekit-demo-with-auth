5f1757c321e6c1e23b1974db43943767f1551ff6
“Himanshu <hranjan120@gmail.com>
2022-08-14 14:16:44 +0530

    dockerfile added

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -19,7 +19,6 @@
 	}
 </script>
 
-<!-- This example requires Tailwind CSS v2.0+ -->
 <nav class="bg-gray-800">
 	<div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
 		<div class="relative flex items-center justify-between h-16">
