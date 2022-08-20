74cf3bcbe128a2f615ee98de2c8d45f805c39c48
“Himanshu <hranjan120@gmail.com>
2022-08-20 18:05:44 +0530

    first cmt

---
+++ src/components/PageTransition.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { fly } from 'svelte/transition';
+	export let pathname = '';
+</script>
+
+<!-- Whenever pathname changes, unmount (page transition out), then re-mount (page transition in) -->
+{#key pathname}
+	<div in:fly={{ x: -2, duration: 200, delay: 200 }} out:fly={{ x: 2, duration: 200 }}>
+		<slot />
+	</div>
+{/key}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,17 @@
+<script context="module">
+	export const load = async ({ url: { pathname } }) => ({
+		props: { pathname }
+	});
+</script>
+
 <script>
+	import PageTransition from '../components/PageTransition.svelte';
 	import '../app.css';
 	import Header from '../components/Header.svelte';
+	export let pathname;
 </script>
 
 <Header />
-<slot />
+<PageTransition {pathname}>
+	<slot />
+</PageTransition>
